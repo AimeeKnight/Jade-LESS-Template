7be1ac097a731532f128bb56d4acd5572bdce8a6
Aimee Knight <AimeeKnight83@gmail.com>
2014-01-21 09:18:11 -0600

    add logging to app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+var http = require('http');
 var connect = require('connect');
+var directory = 'public';
+var port = 3000;
 
-connect.createServer(
-  connect.static(__dirname + '/public')
-).listen(3000);
+var app = connect()
+  .use(connect.logger('dev'))
+  .use(connect.static(directory));
+
+http.createServer(app).listen(port, function(){
+  console.log('Node server listening on port ' + port);
+});
 
